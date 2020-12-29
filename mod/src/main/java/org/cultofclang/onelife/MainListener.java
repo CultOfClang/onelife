f7b4ba498a7a6717fa99ff5a6bd81790f5458d6d
LizeLive <github@lize.live>
2020-12-29 15:57:17 -0800

    making mod

---
+++ mod/src/main/java/org/cultofclang/onelife/MainListener.java
@@ -0,0 +1,18 @@
+package org.cultofclang.onelife;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class MainListener implements Listener {
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event){
+        Player player = event.getPlayer();
+        Location playerLoc = player.getLocation();
+        player.sendMessage("Your X Coordinates : " + playerLoc.getX());
+        player.sendMessage("Your Y Coordinates : " + playerLoc.getY());
+        player.sendMessage("Your Z Coordinates : " + playerLoc.getZ());
+    }
+}
---
+++ mod/src/main/java/org/cultofclang/onelife/OneLifePlugin.java
@@ -0,0 +1,24 @@
+package org.cultofclang.onelife;
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class OneLifePlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Their can only be one.");
+        PluginManager pm = Bukkit.getPluginManager();
+        pm.registerEvents(new MainListener(), this);
+        //getCommand("onelife").setExecutor(new );
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        getLogger().info("hello");
+        return super.onCommand(sender, command, label, args);
+    }
+}
